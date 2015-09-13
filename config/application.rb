96125b5673b22cf8461979af5b1653009e319f91
José Dulanto <jose.dulanto@live.com>
2015-09-13 18:36:49 -0500

    adds instructions for application.yml file needed by figaro, also config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,6 @@ module RailsBoilerplate
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
     
-    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+    config.cache_store = :redis_store, ENV["REDIS_SERVER_URL"], { expires_in: 90.minutes }
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :redis_store, servers: 'redis://localhost:6379/0/cache'
+Rails.application.config.session_store :redis_store, servers: ENV["REDIS_SERVER_URL"]
