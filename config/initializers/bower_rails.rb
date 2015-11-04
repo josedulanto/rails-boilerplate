a423759dba2fee5b6a9e92a3a5f55c829e29d26c
José Dulanto <jose.dulanto@live.com>
2015-11-04 01:45:53 -0600

    this was the line to be commented...

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -12,14 +12,14 @@ BowerRails.configure do |bower_rails|
   bower_rails.clean_before_precompile = true
 
   # Excludes specific bower components from clean. Defaults to nil
-  # bower_rails.exclude_from_clean = ['moment']
+  bower_rails.exclude_from_clean = ['moment']
 
   # Invokes rake bower:install:deployment instead of rake bower:install. Defaults to false
   bower_rails.use_bower_install_deployment = true
 
   # rake bower:install will search for gem dependencies and in each gem it will search for Bowerfile
   # and then concatenate all Bowerfile for evaluation
-  bower_rails.use_gem_deps_for_bowerfile = true
+  # bower_rails.use_gem_deps_for_bowerfile = true
 
   # Passes the -F option to rake bower:install or rake bower:install:deployment. Defaults to false.
   bower_rails.force_install = true
