3c1c1ad835a0247f52dedf5aa4890b460a569b0c
José Dulanto <jose.dulanto@live.com>
2015-11-04 01:28:08 -0600

    installs and resolves bower packages before precompile

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -3,10 +3,10 @@ BowerRails.configure do |bower_rails|
   # bower_rails.root_path = Dir.pwd
 
   # Invokes rake bower:install before precompilation. Defaults to false
-  # bower_rails.install_before_precompile = true
+  bower_rails.install_before_precompile = true
 
   # Invokes rake bower:resolve before precompilation. Defaults to false
-  # bower_rails.resolve_before_precompile = true
+  bower_rails.resolve_before_precompile = true
 
   # Invokes rake bower:clean before precompilation. Defaults to false
   # bower_rails.clean_before_precompile = true
